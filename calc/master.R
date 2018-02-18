810ca615aaaffb31e68c0e2d26a2166e265a2630
pwkraft <kraft.pw@gmail.com>
2018-02-18 12:30:01 -0500

    comments jenn, remove old intro

--- calc/master.R
+++ calc/master.R
@@ -4,6 +4,7 @@ source("/home/patrick/Dropbox/Uni/Projects/2016/knowledge/calc/prep_anes2016.R"
        , echo=T, max.deparse.length=10000)
 source("/home/patrick/Dropbox/Uni/Projects/2016/knowledge/calc/prep_yougov.R"
        , echo=T, max.deparse.length=10000)
+## continue here
 source("/home/patrick/Dropbox/Uni/Projects/2016/knowledge/calc/prep_swiss.R"
        , echo=T, max.deparse.length=10000)
 source("/home/patrick/Dropbox/Uni/Projects/2016/knowledge/calc/analyses_anes.R"
@@ -11,6 +12,10 @@ source("/home/patrick/Dropbox/Uni/Projects/2016/knowledge/calc/analyses_anes.R"
 source("/home/patrick/Dropbox/Uni/Projects/2016/knowledge/calc/analyses_yougov.R"
        , echo=T, max.deparse.length=10000)
 source("/home/patrick/Dropbox/Uni/Projects/2016/knowledge/calc/analyses_swiss.R"
+       , echo=T, max.deparse.length=10000)
+source("/home/patrick/Dropbox/Uni/Projects/2016/knowledge/calc/analyses_ggap.R"
+       , echo=T, max.deparse.length=10000)
+source("/home/patrick/Dropbox/Uni/Projects/2016/knowledge/calc/analyses_pretext.R"
        , echo=T, max.deparse.length=10000)
 source("/home/patrick/Dropbox/Uni/Projects/2016/knowledge/calc/appendix_anes.R"
        , echo=T, max.deparse.length=10000)
