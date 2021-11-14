ea3a58c333f7854247a9c8dbc0d023bc9dbc5c0d
Patrick Kraft <kraft.pw@gmail.com>
2021-11-14 17:25:13 -0600

    update swiss recoding

--- calc/prep_swiss.R
+++ calc/prep_swiss.R
@@ -21,9 +21,11 @@ library(quanteda.dictionaries)
 library(stm)
 library(haven)
 library(ineq)
-setwd("/data/Dropbox/Uni/Projects/2016/knowledge/")
 datasrc <- "/home/patrick/Dropbox/Uni/Data/colombo/"
 raw <- read_dta(paste0(datasrc,"citizencompetence_colombo.dta"), encoding = "latin 1")
+liwc_de <- dictionary(file = "~/Dropbox/Uni/Data/LIWC/German_LIWC2001_Dictionary.dic", format = "LIWC")
+liwc_fr <- dictionary(file = "~/Dropbox/Uni/Data/LIWC/French_LIWC2007_Dictionary.dic", format = "LIWC", encoding = "LATIN1")
+liwc_it <- dictionary(file = "~/Dropbox/Uni/Data/LIWC/Italian_LIWC2007_Dictionary.dic", format = "LIWC", encoding = "LATIN1")
 
 source("calc/func.R")
 
@@ -64,12 +66,8 @@ swiss$prostring2[swiss$prostring2 %in% c(99999997,99999998)] <- ""
 swiss$constring1[swiss$constring1 %in% c(99999997,99999998)] <- ""
 swiss$constring2[swiss$constring2 %in% c(99999997,99999998)] <- ""
 
-## combine pro and con
-swiss$string1 <- paste(swiss$prostring1, swiss$constring1)
-swiss$string2 <- paste(swiss$prostring2, swiss$constring2)
-
 ## minor pre-processing
-opend <- apply(swiss[,c("string1","string2")], 2, function(x){
+preprocess <- function(x){
   x <- char_tolower(x)
   x <- gsub("(^\\s+|\\s+$)","", x)
   x[x %in% c("no","non","nein","rien","nichts","keinen","nsp","ka","n","prive","nono"
@@ -84,153 +82,240 @@ opend <- apply(swiss[,c("string1","string2")], 2, function(x){
   x <- gsub("\\s+"," ", x)
   x <- gsub("(^\\s+|\\s+$)","", x)
   return(x)
-  })
-
-
-### add meta information about responses
-
-## function to compute shannon entropy (rescaled to 0-1??)
-shannon <- function(x, reversed = F){
-  out <- (- sum(log(x^x)/log(length(x))))
-  if(reversed) out <- 1 - out
-  out
 }
 
-## overall response length (this should actually be done by language!!!)
-swiss$wc <- apply(opend, 1, function(x){
-  length(unlist(strsplit(x,"\\s+")))
+## combine pro and con
+swiss <- swiss %>%
+  mutate(
+    string1 = preprocess(paste(prostring1, constring1)),
+    string2 = preprocess(paste(prostring2, constring2))
+  )
+
+
+
+# GERMAN: Text-based political sophistication measure ----------------------------
+
+opend_german <- swiss %>% filter(lang == 1)
+
+
+## Consistency: Shannon entropy of response lengths ----------------------
+
+## overall response length
+opend_german$wc <- apply(dplyr::select(opend_german, string1:string2), 1, function(x){
+  sum(str_count(x, "\\w+"))
 })
-swiss$lwc <- log(swiss$wc)/max(log(swiss$wc))
+opend_german$lwc <- log(opend_german$wc)/max(log(opend_german$wc), na.rm = T)
 
-## number of items answered
-swiss$nitem <- apply(opend != "", 1, sum, na.rm = T)
-
-## diversity in item response (respondents either answered pro OR con, I should take that into account!!!)
-swiss$ditem <- apply(opend, 1, function(x){
-  iwc <- unlist(lapply(strsplit(x,"\\s+"), length))
+### consistency in item response  (respondents either answered pro OR con, I should take that into account!!!)
+opend_german$consistency <- apply(dplyr::select(opend_german, string1:string2), 1, function(x){
+  iwc <- str_count(x, "\\w+")
   shannon(iwc/sum(iwc))
 })
 
 
-### fit structural topic model
-
-### prepare data
+## Considerations: Number of topics mentioned -----------------------------
 
 ## combine regular survey and open-ended data
 ## ADD: type of referendum as a meta-covariate
 meta <- c("age", "edu", "ideol", "edu_ideol", "female")
-swiss$resp <- apply(dplyr::select(opend, string1:string2),1,paste,collapse=' ')
+opend_german$resp <- apply(dplyr::select(opend_german, string1:string2),1,paste,collapse=' ')
 
 ## remove additional whitespaces
-swiss$resp <- gsub("\\s+"," ", swiss$resp)
-swiss$resp <- gsub("(^\\s+|\\s+$)","", swiss$resp)
+opend_german$resp <- gsub("\\s+"," ", opend_german$resp)
+opend_german$resp <- gsub("(^\\s+|\\s+$)","", opend_german$resp)
 
 ## remove missings on metadata
-data <- swiss[apply(!is.na(swiss[,meta]),1,prod)==1,]
+opend_german <- opend_german[apply(!is.na(opend_german[,meta]),1,prod)==1, ]
 
 ## remove empty strings
-data <- data[data$wc != 0, ]
+opend_german <- opend_german[opend_german$wc != 0, ]
 
+### process for stm
+processed <- textProcessor(opend_german$resp, metadata = opend_german[,meta],
+                           language = "german")
+out_german <- prepDocuments(processed$documents, processed$vocab, processed$meta,
+                            lower.thresh = 10)
 
-
-###################################################
-### German respondents
-
-## select language
-opend_german <- data %>% filter(lang == 1)
-
-## process for stm
-processed <- textProcessor(opend_german$resp, metadata = opend_german[,meta]
-                           , language = "german")
-out_german <- prepDocuments(processed$documents, processed$vocab, processed$meta
-                            , lower.thresh = 10)
-
-## remove discarded observations from data
+### remove discarded observations from data
 opend_german <- opend_german[-processed$docs.removed,]
 opend_german <- opend_german[-out_german$docs.removed,]
 
-## quick fit (30 topics)
-stm_fit_german <- stm(out_german$documents, out_german$vocab, prevalence = as.matrix(out_german$meta)
-                      , K=49, seed=12345)
+### quick fit (30 topics)
+stm_fit_german <- stm(out_german$documents, out_german$vocab, prevalence = as.matrix(out_german$meta),
+                      K=49, seed=12345)
+
+### compute number of considerations
+opend_german$considerations <- ntopics(stm_fit_german, out_german)
 
 
-### Discursive sophistication measure
+## Word choice: LIWC component ---------------------------------------------
 
-## combine sophistication components with remaining data
-opend_german <- cbind(opend_german, sophistication(stm_fit_german, out_german))
+opend_german_liwc <- liwcalike(opend_german$resp, liwc_de)
 
-## compute combined measures
-opend_german$polknow_text <- with(opend_german, ntopics * distinct * ditem)
-opend_german$polknow_text_mean <- with(opend_german, ntopics + distinct + ditem)/3
+### combine exclusive words and conjunctions (see Tausczik and Pennebaker 2010: 35)
+### NOTE Conjunctions not included, so I'm using cognative mechanisms
+opend_german$wordchoice <- (opend_german_liwc$cogmech + opend_german_liwc$negate) * opend_german_liwc$WC
+opend_german$wordchoice <- opend_german$wordchoice / max(opend_german$wordchoice)
+
+
+## Merge with full data and save -------------------------------------------
+
+### compute combined measures
+opend_german$polknow_text <- with(opend_german, considerations * consistency * wordchoice)
+opend_german$polknow_text_mean <- with(opend_german, considerations + consistency + wordchoice)/3
 
 
 
-#####################################################
-### French respondents
+# FRENCH: Text-based political sophistication measure --------------------
 
-## select language
-opend_french <- data %>% filter(lang == 2)
+opend_french <- swiss %>% filter(lang == 2)
 
-## process for stm
-processed <- textProcessor(opend_french$resp, metadata = opend_french[,meta]
-                           , language = "french")
-out_french <- prepDocuments(processed$documents, processed$vocab, processed$meta
-                     , lower.thresh = 10)
 
-## remove discarded observations from data
+## Consistency: Shannon entropy of response lengths ----------------------
+
+## overall response length
+opend_french$wc <- apply(dplyr::select(opend_french, string1:string2), 1, function(x){
+  sum(str_count(x, "\\w+"))
+})
+opend_french$lwc <- log(opend_french$wc)/max(log(opend_french$wc), na.rm = T)
+
+### consistency in item response  (respondents either answered pro OR con, I should take that into account!!!)
+opend_french$consistency <- apply(dplyr::select(opend_french, string1:string2), 1, function(x){
+  iwc <- str_count(x, "\\w+")
+  shannon(iwc/sum(iwc))
+})
+
+
+## Considerations: Number of topics mentioned -----------------------------
+
+## combine regular survey and open-ended data
+## ADD: type of referendum as a meta-covariate
+meta <- c("age", "edu", "ideol", "edu_ideol", "female")
+opend_french$resp <- apply(dplyr::select(opend_french, string1:string2),1,paste,collapse=' ')
+
+## remove additional whitespaces
+opend_french$resp <- gsub("\\s+"," ", opend_french$resp)
+opend_french$resp <- gsub("(^\\s+|\\s+$)","", opend_french$resp)
+
+## remove missings on metadata
+opend_french <- opend_french[apply(!is.na(opend_french[,meta]),1,prod)==1, ]
+
+## remove empty strings
+opend_french <- opend_french[opend_french$wc != 0, ]
+
+### process for stm
+processed <- textProcessor(opend_french$resp, metadata = opend_french[,meta],
+                           language = "french")
+out_french <- prepDocuments(processed$documents, processed$vocab, processed$meta,
+                            lower.thresh = 10)
+
+### remove discarded observations from data
 opend_french <- opend_french[-processed$docs.removed,]
 opend_french <- opend_french[-out_french$docs.removed,]
 
 ## quick fit (60 topics)
-stm_fit_french <- stm(out_french$documents, out_french$vocab, prevalence = as.matrix(out_french$meta)
-                      , K=44, seed=12345)
+stm_fit_french <- stm(out_french$documents, out_french$vocab, prevalence = as.matrix(out_french$meta),
+                      K=44, seed=12345)
+
+### compute number of considerations
+opend_french$considerations <- ntopics(stm_fit_french, out_french)
 
 
-### Discursive sophistication measure
+## Word choice: LIWC component ---------------------------------------------
 
-## combine sophistication components with remaining data
-opend_french <- cbind(opend_french, sophistication(stm_fit_french, out_french))
+opend_french_liwc <- liwcalike(opend_french$resp, liwc_de)
 
-## compute combined measures
-opend_french$polknow_text <- with(opend_french, ntopics * distinct * ditem)
-opend_french$polknow_text_mean <- with(opend_french, ntopics + distinct + ditem)/3
+### combine exclusive words and conjunctions (see Tausczik and Pennebaker 2010: 35)
+### NOTE Conjunctions not included, so I'm using cognative mechanisms
+opend_french$wordchoice <- (opend_french_liwc$cogmech + opend_french_liwc$negate) * opend_french_liwc$WC
+opend_french$wordchoice <- opend_french$wordchoice / max(opend_french$wordchoice)
+
+
+## Merge with full data and save -------------------------------------------
+
+### compute combined measures
+opend_french$polknow_text <- with(opend_french, considerations * consistency * wordchoice)
+opend_french$polknow_text_mean <- with(opend_french, considerations + consistency + wordchoice)/3
 
 
 
-#####################################################
-### Italian respondents
+# ITALIAN: Text-based political sophistication measure  --------------------
 
-## select language
-opend_italian <- data %>% filter(lang == 3)
+opend_italian <- swiss %>% filter(lang == 3)
 
-## process for stm
-processed <- textProcessor(opend_italian$resp, metadata = opend_italian[,meta]
-                           , language = "italian")
-out_italian <- prepDocuments(processed$documents, processed$vocab, processed$meta
-                     , lower.thresh = 10)
 
-## remove discarded observations from data
+## Consistency: Shannon entropy of response lengths ----------------------
+
+## overall response length
+opend_italian$wc <- apply(dplyr::select(opend_italian, string1:string2), 1, function(x){
+  sum(str_count(x, "\\w+"))
+})
+opend_italian$lwc <- log(opend_italian$wc)/max(log(opend_italian$wc), na.rm = T)
+
+### consistency in item response  (respondents either answered pro OR con, I should take that into account!!!)
+opend_italian$consistency <- apply(dplyr::select(opend_italian, string1:string2), 1, function(x){
+  iwc <- str_count(x, "\\w+")
+  shannon(iwc/sum(iwc))
+})
+
+
+## Considerations: Number of topics mentioned -----------------------------
+
+## combine regular survey and open-ended data
+## ADD: type of referendum as a meta-covariate
+meta <- c("age", "edu", "ideol", "edu_ideol", "female")
+opend_italian$resp <- apply(dplyr::select(opend_italian, string1:string2),1,paste,collapse=' ')
+
+## remove additional whitespaces
+opend_italian$resp <- gsub("\\s+"," ", opend_italian$resp)
+opend_italian$resp <- gsub("(^\\s+|\\s+$)","", opend_italian$resp)
+
+## remove missings on metadata
+opend_italian <- opend_italian[apply(!is.na(opend_italian[,meta]),1,prod)==1, ]
+
+## remove empty strings
+opend_italian <- opend_italian[opend_italian$wc != 0, ]
+
+### process for stm
+processed <- textProcessor(opend_italian$resp, metadata = opend_italian[,meta],
+                           language = "italian")
+out_italian <- prepDocuments(processed$documents, processed$vocab, processed$meta,
+                            lower.thresh = 10)
+
+### remove discarded observations from data
 opend_italian <- opend_italian[-processed$docs.removed,]
 opend_italian <- opend_italian[-out_italian$docs.removed,]
 
 ## quick fit (60 topics)
-stm_fit_italian <- stm(out_italian$documents, out_italian$vocab, prevalence = as.matrix(out_italian$meta)
-                       , K=36, seed=12345)
+stm_fit_italian <- stm(out_italian$documents, out_italian$vocab, prevalence = as.matrix(out_italian$meta),
+                      K=36, seed=12345)
+
+### compute number of considerations
+opend_italian$considerations <- ntopics(stm_fit_italian, out_italian)
 
 
-### Discursive sophistication measure
+## Word choice: LIWC component ---------------------------------------------
 
-## combine sophistication components with remaining data
-opend_italian <- cbind(opend_italian, sophistication(stm_fit_italian, out_italian))
+opend_italian_liwc <- liwcalike(opend_italian$resp, liwc_de)
 
-## compute combined measures
-opend_italian$polknow_text <- with(opend_italian, ntopics * distinct * ditem)
-opend_italian$polknow_text_mean <- with(opend_italian, ntopics + distinct + ditem)/3
+### combine exclusive words and conjunctions (see Tausczik and Pennebaker 2010: 35)
+### NOTE Conjunctions not included, so I'm using cognative mechanisms
+opend_italian$wordchoice <- (opend_italian_liwc$cogmech + opend_italian_liwc$negate) * opend_italian_liwc$WC
+opend_italian$wordchoice <- opend_italian$wordchoice / max(opend_italian$wordchoice)
 
 
-### save output
+## Merge with full data and save -------------------------------------------
 
-save(swiss, opend_german, opend_french, opend_italian, meta, processed
-     , out_german, out_french, out_italian
-     , stm_fit_german, stm_fit_french, stm_fit_italian
-     , file="calc/out/swiss.Rdata")
+### compute combined measures
+opend_italian$polknow_text <- with(opend_italian, considerations * consistency * wordchoice)
+opend_italian$polknow_text_mean <- with(opend_italian, considerations + consistency + wordchoice)/3
+
+
+
+
+# save output -------------------------------------------------------------
+
+save(swiss, opend_german, opend_french, opend_italian, meta, processed,
+     out_german, out_french, out_italian,
+     stm_fit_german, stm_fit_french, stm_fit_italian,
+     file="calc/out/swiss.Rdata")
