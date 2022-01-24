a4afac7f431996f6e2fbe1b378259590254d802b
pwkraft <kraft.pw@gmail.com>
2022-01-24 15:52:54 -0600

    update master.R, remove old files

--- calc/master.R
+++ calc/master.R
@@ -1,27 +1,16 @@
-source("~/Dropbox/Uni/projects/2016/knowledge/calc/prep_anes2012.R",
+source("calc/prep_cces.R",
        echo=T, max.deparse.length=10000)
-source("~/Dropbox/Uni/projects/2016/knowledge/calc/prep_anes2016.R",
+source("calc/prep_anes2020.R",
        echo=T, max.deparse.length=10000)
-source("~/Dropbox/Uni/projects/2016/knowledge/calc/prep_anes2020.R",
+source("calc/prep_anes2016.R",
        echo=T, max.deparse.length=10000)
-source("~/Dropbox/Uni/projects/2016/knowledge/calc/prep_yougov.R",
+source("calc/prep_anes2012.R",
        echo=T, max.deparse.length=10000)
-source("~/Dropbox/Uni/projects/2016/knowledge/calc/prep_swiss.R",
+source("calc/prep_yougov.R",
        echo=T, max.deparse.length=10000)
-## continue here
-source("~/Dropbox/Uni/projects/2016/knowledge/calc/analyses_anes.R",
+source("calc/prep_swiss.R",
        echo=T, max.deparse.length=10000)
-source("~/Dropbox/Uni/projects/2016/knowledge/calc/analyses_yougov.R",
+source("calc/analyses.R",
        echo=T, max.deparse.length=10000)
-source("~/Dropbox/Uni/projects/2016/knowledge/calc/analyses_swiss.R",
-       echo=T, max.deparse.length=10000)
-source("~/Dropbox/Uni/projects/2016/knowledge/calc/analyses_ggap.R",
-       echo=T, max.deparse.length=10000)
-source("~/Dropbox/Uni/projects/2016/knowledge/calc/appendix_anes.R",
-       echo=T, max.deparse.length=10000)
-source("~/Dropbox/Uni/projects/2016/knowledge/calc/appendix_yougov.R",
-       echo=T, max.deparse.length=10000)
-source("~/Dropbox/Uni/projects/2016/knowledge/calc/appendix_swiss.R",
-       echo=T, max.deparse.length=10000)
-source("~/Dropbox/Uni/projects/2016/knowledge/calc/analyses_pretext.R",
+source("calc/appendix.R",
        echo=T, max.deparse.length=10000)
